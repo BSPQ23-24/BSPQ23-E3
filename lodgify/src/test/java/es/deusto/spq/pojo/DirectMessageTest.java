4017f3ab06729b84c6fd4dcec59c189880da38a2
Unai Alonso <unai.alonso@opendeusto.es>
2024-05-01 09:51:23 +0200

    Direct Message Test properly functioning

--- lodgify/src/test/java/es/deusto/spq/pojo/DirectMessageTest.java
+++ lodgify/src/test/java/es/deusto/spq/pojo/DirectMessageTest.java
@@ -1,37 +1,54 @@
 package es.deusto.spq.pojo;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
-
 
 public class DirectMessageTest {
 
     DirectMessage directMessage;
-
-    @Mock
     UserData userData;
-
-    @Mock
     MessageData messageData;
 
     @Before
     public void setUp() {
+
+        userData = mock(UserData.class);
+        messageData = mock(MessageData.class);
+
         directMessage = new DirectMessage();
         directMessage.setUserData(userData);
         directMessage.setMessageData(messageData);
     }
 
     @Test
-    public void getUserData() {
+    public void testGetUserData() {
         assertEquals(userData, directMessage.getUserData());
     }
 
     @Test
-    public void getMessageData() {
+    public void testGetMessageData() {
         assertEquals(messageData, directMessage.getMessageData());
+    }
+
+    @Test
+    public void testSetUserData() {
+        UserData newUserData = mock(UserData.class);
+        
+        directMessage.setUserData(newUserData);
+        
+        assertEquals(newUserData, directMessage.getUserData());
+    }
+
+    @Test
+    public void testSetMessageData() {
+        MessageData newMessageData = mock(MessageData.class);
+        
+        directMessage.setMessageData(newMessageData);
+        
+        assertEquals(newMessageData, directMessage.getMessageData());
     }
 }
 
