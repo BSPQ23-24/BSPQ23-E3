fa78bb267e6ef338ae1ce0bad1d16d34e4671ccc
Unai Alonso <unai.alonso@opendeusto.es>
2024-05-20 13:15:15 +0200

    Payment Icon update

--- lodgify/lodgify-frontend/lodgify-frontend/src/Bookings.jsx
+++ lodgify/lodgify-frontend/lodgify-frontend/src/Bookings.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import en from "./translations/en.json";
 import es from "./translations/es.json";
 import lt from "./translations/lt.json";
@@ -13,6 +14,7 @@ import { format, differenceInDays } from 'date-fns';
 function Bookings() {
   const [bookings, setBookings] = useState([]);
   const [residences, setResidences] = useState({});
+  const navigate = useNavigate();
   const [headerText, setHeaderText] = useState("");
   const [refreshNeeded, setRefreshNeeded] = useState(false);
   const [showConfirmation, setShowConfirmation] = useState(false);
@@ -221,6 +223,14 @@ function Bookings() {
                   />
                 </svg>
               </button>
+            </div>
+            <div className="p-10">
+              {/* <button onClick={(e) => removeBooking(booking.id)}> */}
+              <button onClick={() => navigate("/payment")}>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                  </svg>
+                </button>
             </div>
             {showConfirmation && (
               <Confirmation
